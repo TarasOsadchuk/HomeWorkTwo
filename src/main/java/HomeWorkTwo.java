fc8e59999954a057cabb5447ea4aa123c99d1685
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2021-11-22 15:01:18 +0200

    Исправил

--- src/main/java/HomeWorkTwo.java
+++ src/main/java/HomeWorkTwo.java
@@ -2,22 +2,22 @@
 public class HomeWorkTwo {
     public static void main(String[] args) {
 
-        twoNum(8,5); // solution of the first problem
+        System.out.println(twoNum(8, 5)); // solution of the first problem
         whichNumOne(0); // solution of the 2nd problem
-        whichNumTwo(-2); // solution of the 3rd problem
+        System.out.println(whichNumTwo(-2)); // solution of the 3rd problem
         numberOfTime("Hello GeekBrains", 5); // solution of the 4th problem
-        leapYear(2021); // last task 5
+        System.out.println(leapYear(2021)); // last task 5
 
     }
 
-    public static void twoNum(int numOne, int numTwo) {
+    public static boolean twoNum(int numOne, int numTwo) {
 
         int sum = numOne + numTwo;
 
         if (sum >= 10 && sum <= 20) {
-            System.out.println(true);
+            return true;
         } else {
-            System.out.println(false);
+            return false;
         }
     } // N1
 
@@ -30,12 +30,12 @@ public class HomeWorkTwo {
         }
     } // N2
 
-    public static void whichNumTwo(int num) {
+    public static boolean whichNumTwo(int num) {
 
         if (num < 0) {
-            System.out.println(true);
+            return true;
         } else {
-            System.out.println(false);
+            return false;
         }
     } // N3
 
@@ -46,16 +46,16 @@ public class HomeWorkTwo {
         }
     } // N4
 
-    public static void leapYear(int year) {
+    public static boolean leapYear(int year) {
 
         if ((year % 400) == 0) {
-            System.out.println(true);
+            return true;
         } else if ((year % 100) == 0) {
-            System.out.println(false);
+            return false;
         } else if ((year % 4) == 0) {
-            System.out.println(true);
+            return true;
         } else {
-            System.out.println(false);
+            return false;
         }
 
     } //N5
